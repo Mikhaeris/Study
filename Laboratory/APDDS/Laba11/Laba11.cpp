195d1e2355bc715ec76d5da0c8b833063ca17a5f
Mikhaeris <mihail_kiselev00@mail.ru>
2025-03-13 21:28:13 +0300

    laba11 in APDDS

--- Laboratory/APDDS/Laba11/Laba11.cpp
+++ Laboratory/APDDS/Laba11/Laba11.cpp
@@ -5,65 +5,75 @@
 
 using namespace std;
 
-map<char, int> characterNumber;
+map<char, int> characterNumber = { {'A',0}, {'C',1}, {'G',2}, {'T',3} };
+map<int, char> characterNumberRev = { {0, 'A'}, {1, 'C'}, {2, 'G'}, {3, 'T'} };
 
 void addInVec(vector<vector<int>>& repeatingCharactersInf, char tempCharacter, int countTempCharacter) {
 	int numberCharacterInMap = characterNumber[tempCharacter];
-	if (repeatingCharactersInf[numberCharacterInMap].size() >= countTempCharacter) {
-		repeatingCharactersInf[numberCharacterInMap][countTempCharacter - 1] += 1;
+	if (repeatingCharactersInf.size() >= countTempCharacter) {
+		repeatingCharactersInf[countTempCharacter - 1][numberCharacterInMap] += 1;
 	}
-	else if (repeatingCharactersInf[numberCharacterInMap].size() < countTempCharacter) {
+	else if (repeatingCharactersInf.size() < countTempCharacter) {
 		vector<int> tempVec = { 0, 0, 0, 0 };
 		for (int i = repeatingCharactersInf.size(); i < countTempCharacter; i++) {
 			repeatingCharactersInf.push_back(tempVec);
 		}
-		repeatingCharactersInf[numberCharacterInMap][countTempCharacter - 1] += 1;
+		repeatingCharactersInf[countTempCharacter - 1][numberCharacterInMap] += 1;
 	}
 }
 
 pair<char, int> goNextSpace(string& str, int index) {
 	for (int i = index; i < str.size() - 1; i++) {
 		if (str[i + 1] != ' ') {
-			return pair<char, int>(str[i], i);
+			return pair<char, int>(str[i + 1], i);
 		}
 	}
+	return pair<char, int>(str[0], str.size());
 }
 
 void countCharacters(vector<vector<int>>& repeatingCharactersInf, string& str) {
 	char tempCharacter = str[0]; int countTempCharacter = 0;
 	for (int i = 0; i < str.size() - 1; i++) {
+		countTempCharacter++;
 		if (str[i + 1] != tempCharacter) {
 			addInVec(repeatingCharactersInf, tempCharacter, countTempCharacter);
 			if (str[i + 1] != ' ') {
 				tempCharacter = str[i + 1];
 			}
 			else {
-				auto infCharacterAndCountSpace = goNextSpace(str, i + 1);
+				auto infCharacterAndCountSpace = goNextSpace(str, i);
 				tempCharacter = infCharacterAndCountSpace.first;
 				i = infCharacterAndCountSpace.second;
 			}
 			countTempCharacter = 0;
 		}
-		countTempCharacter++;
 	}
 }
 
 int main() {
-	characterNumber.insert(make_pair('A', 0));
-	characterNumber.insert(make_pair('C', 1));
-	characterNumber.insert(make_pair('G', 2));
-	characterNumber.insert(make_pair('T', 3));
+	vector<vector<int>> repeatingCharactersInf(0, vector<int>(4));
 
-	vector<vector<int>> repeatingCharactersInf;
-
-	string str; cout << "Enter str: "; cin.ignore(); getline(cin, str);
+	string str; cout << "Enter str: "; getline(cin, str); str.push_back(' ');
 	int n = 0; cout << "Enter n: "; cin >> n;
 
 	countCharacters(repeatingCharactersInf, str);
 
-	for (int i = n; i < repeatingCharactersInf[n].size(); i++) {
-		if (repeatingCharactersInf[n][i] == n) {
-			cout << repeatingCharactersInf[n][i] << endl;
+	bool tempBreak = 0;
+	for (int i = repeatingCharactersInf.size() - 1; i > 1; i--) {
+		for (int j = 0; j < 4; j++) {
+			if (repeatingCharactersInf[i][j] == n) {
+				char character = characterNumberRev[j];
+				for (int k = 0; k < i + 1; k++) {
+					cout << character;
+				}
+				cout << endl;
+				cout << "Count character: " << n << "\nWhat character(0-A...): " << character << "\nSize character: " << i + 1 << endl;
+				tempBreak = 1;
+				break;
+			}
+		}
+		if (tempBreak == 1) {
+			break;
 		}
 	}
 }
