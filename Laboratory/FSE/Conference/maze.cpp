f1ef43c0bc1f10a6bd1ec8dbb9d5dc4aa0536d10
Mikhaeris <mihail_kiselev00@mail.ru>
2025-03-18 00:10:02 +0300

    create conference folder

---
+++ Laboratory/FSE/Conference/maze.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Maze
+void generateMaze() {
+
+}
+
+void enterMaze() {
+
+}
+
+void mazeToGraph() {
+
+}
+
+void graphToMaze() {
+
+}
+
+void printMaze() {
+
+}
+
+// Bfs
+void bfs() {
+	//return minimal path from start to exit
+}
+
+
+
+int main() {
+	int n = 10;
+	//graph representation as an adjacency list
+	vector<vector<int>> adjacencyList(n);
+
+
+
+	return 0;
+}
