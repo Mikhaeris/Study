1b9184e33446f1351503174c39a3c11b013b0d42
Mikhaeris <mikhail.kiselev.dev@gmail.com>
2025-03-20 15:35:37 +0300

    somw changes

--- Laboratory/APDDS/Laba11/Laba11.cpp
+++ Laboratory/APDDS/Laba11/Laba11.cpp
@@ -9,7 +9,10 @@ map<char, int> characterNumber = { {'A',0}, {'C',1}, {'G',2}, {'T',3} };
 map<int, char> characterNumberRev = { {0, 'A'}, {1, 'C'}, {2, 'G'}, {3, 'T'} };
 
 void addInVec(vector<vector<int>>& repeatingCharactersInf, char tempCharacter, int countTempCharacter) {
-	int numberCharacterInMap = characterNumber[tempCharacter];
+	int numberCharacterInMap;
+	if (tempCharacter != ' ')  numberCharacterInMap = characterNumber[tempCharacter];
+	else return;
+
 	if (repeatingCharactersInf.size() >= countTempCharacter) {
 		repeatingCharactersInf[countTempCharacter - 1][numberCharacterInMap] += 1;
 	}
@@ -50,30 +53,60 @@ void countCharacters(vector<vector<int>>& repeatingCharactersInf, string& str) {
 	}
 }
 
+string genereteString() {
+	map<int, char> mapCharactes;
+	mapCharactes[0] = ' ';
+	mapCharactes[1] = 'A';
+	mapCharactes[2] = 'C';
+	mapCharactes[3] = 'G';
+	mapCharactes[4] = 'T';
+
+	int countStr = 1 + rand() % 100;
+
+	string str;
+	/*for (int i = 0; i < countStr; i++) {
+		int randCharacter = 0 + rand() % 5;
+		str.push_back(mapCharactes[randCharacter]);
+	}*/
+	//str = "ACGTACGTchsdcgsdgksjjswACGT";
+	str = "AAAC shfhsd CCCC sdahdj AAA";
+	return str;
+}
+
 int main() {
+	srand(time(0));
 	vector<vector<int>> repeatingCharactersInf(0, vector<int>(4));
 
-	string str; cout << "Enter str: "; getline(cin, str); str.push_back(' ');
-	int n = 0; cout << "Enter n: "; cin >> n;
+	//string str; cout << "Enter str: "; getline(cin, str); str.push_back(' ');
+	string str = genereteString(); cout << "String: " << str << endl;
 
 	countCharacters(repeatingCharactersInf, str);
 
-	bool tempBreak = 0;
-	for (int i = repeatingCharactersInf.size() - 1; i > 1; i--) {
-		for (int j = 0; j < 4; j++) {
-			if (repeatingCharactersInf[i][j] == n) {
-				char character = characterNumberRev[j];
-				for (int k = 0; k < i + 1; k++) {
-					cout << character;
+
+	while (true) {
+		int n = 0; cout << "Enter n: "; cin >> n;
+		bool tempBreak = 0;
+		for (int i = repeatingCharactersInf.size() - 1; i >= 0; i--) {
+			for (int j = 0; j < 4; j++) {
+				if (repeatingCharactersInf[i][j] == n) {
+					char character = characterNumberRev[j];
+					cout << "str: ";
+					for (int k = 0; k < i + 1; k++) {
+						cout << character;
+					}
+					cout << endl;
+					cout << "Count character: " << n << "\nWhat character(0-A...): " << character << "\nSize character: " << i + 1 << endl;
+					tempBreak = 1;
+					break;
 				}
-				cout << endl;
-				cout << "Count character: " << n << "\nWhat character(0-A...): " << character << "\nSize character: " << i + 1 << endl;
-				tempBreak = 1;
+			}
+			if (tempBreak == 1) {
 				break;
 			}
 		}
-		if (tempBreak == 1) {
-			break;
+		if (tempBreak == 0) {
+			cout << "0" << endl;
 		}
+		cout << "-------------" << endl;
 	}
 }
