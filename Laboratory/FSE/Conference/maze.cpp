c97ca1aa6b3bdb8d941327b3282cb54cb0f29c79
Mikhaeris <mikhail.kiselev.dev@gmail.com>
2025-04-05 23:16:00 +0300

    some changes pptx in FSE

--- Laboratory/FSE/Conference/maze.cpp
+++ Laboratory/FSE/Conference/maze.cpp
@@ -2,11 +2,8 @@
 #include <vector>
 #include <set>
 #include <queue>
-#include <tuple>
 #include <algorithm>
 #include <windows.h>
-
-using namespace std;
 
 class Graph {
 protected:
@@ -15,7 +12,7 @@ protected:
 public:
 	Graph() = default;
 
-	virtual void mazeToGraph(const vector<vector<int>>& maze) = 0;
+	virtual void convertMazeToGraph(const std::vector<std::vector<int>>& maze) = 0;
 
 	virtual void printGraph() const = 0;
 
@@ -23,31 +20,31 @@ public:
 
 class WeightedGraph : public Graph {
 private:
-	vector<vector<pair<int, int>>> adjacencyListWithWeight;
+	std::vector<std::vector<std::pair<int, int>>> adjacencyListWithWeight;
 
 public:
 
 	WeightedGraph() = default;
 
-	void mazeToGraph(const vector<vector<int>>& startMatrix) override {
+	void convertMazeToGraph(const std::vector<std::vector<int>>& startMatrix) override {
 		height = startMatrix.size();
 		width = startMatrix[0].size();
 
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				vector<pair<int, int>> tempVertexadjacencyListWithWeight;
+				std::vector<std::pair<int, int>> tempVertexadjacencyListWithWeight;
 				if (startMatrix[i][j] == 0) {
 					if (j - 1 >= 0 && startMatrix[i][j - 1] == 0) {
-						tempVertexadjacencyListWithWeight.push_back(make_pair(i * width + (j - 1), 1 + rand() % 11));
+						tempVertexadjacencyListWithWeight.push_back(std::make_pair(i * width + (j - 1), 1 + rand() % 11));
 					}
 					if (i - 1 >= 0 && startMatrix[i - 1][j] == 0) {
-						tempVertexadjacencyListWithWeight.push_back(make_pair((i - 1) * width + j, 1 + rand() % 11));
+						tempVertexadjacencyListWithWeight.push_back(std::make_pair((i - 1) * width + j, 1 + rand() % 11));
 					}
 					if (j + 1 < width && startMatrix[i][j + 1] == 0) {
-						tempVertexadjacencyListWithWeight.push_back(make_pair(i * width + (j + 1), 1 + rand() % 11));
+						tempVertexadjacencyListWithWeight.push_back(std::make_pair(i * width + (j + 1), 1 + rand() % 11));
 					}
 					if (i + 1 < height && startMatrix[i + 1][j] == 0) {
-						tempVertexadjacencyListWithWeight.push_back(make_pair((i + 1) * width + j, 1 + rand() % 11));
+						tempVertexadjacencyListWithWeight.push_back(std::make_pair((i + 1) * width + j, 1 + rand() % 11));
 					}
 				}
 				adjacencyListWithWeight.push_back(tempVertexadjacencyListWithWeight);
@@ -55,96 +52,56 @@ public:
 		}
 	}
 
-	/*pair<vector<vector<int>>, pair<int, int>> mstGraphTest() {
-		int n = height*width;
+	std::pair<std::vector<std::vector<int>>, std::pair<int, int>> mstGraph() {
+		int n = height * width;
 		const int INF = INT_MAX;
 
-		vector<int> min_e(n, INF), sel_e(n, -1);
-		min_e[0] = 0;
-		set < pair<int, int> > q;
-		q.insert(make_pair(0, 0));
+		std::vector<std::vector<int>> MST(n);
+
+		std::vector<int> minEdgeWeights(n, INF), parent(n, -1);
+		minEdgeWeights[0] = 0;
+
+		std::vector<bool> used(n, false);
+		std::set<std::pair<int, int>> pQueue;
+
+		pQueue.insert(std::make_pair(0, 0));
 		for (int i = 0; i < n; ++i) {
-			if (q.empty()) {
-				cout << "No MST!";
-				exit(0);
-			}
-			int v = q.begin()->second;
-			q.erase(q.begin());
-
-			if (sel_e[v] != -1)
-				cout << v << " " << sel_e[v] << endl;
-
-			for (size_t j = 0; j < adjacencyListWithWeight[v].size(); ++j) {
-				int to = adjacencyListWithWeight[v][j].first,
-					cost = adjacencyListWithWeight[v][j].second;
-				if (cost < min_e[to]) {
-					q.erase(make_pair(min_e[to], to));
-					min_e[to] = cost;
-					sel_e[to] = v;
-					q.insert(make_pair(min_e[to], to));
-				}
-			}
-		}
-	}*/
-
-	pair<vector<vector<int>>, pair<int, int>> mstGraph() {
-		int n = height * width;
-		const int INF = 1000000000;
-
-		vector<int> min_e(n, INF), sel_e(n, -1);
-		vector<bool> visited(n, false);
-		min_e[0] = 0;
-
-		vector<vector<int>> MST(n);
-
-		set<pair<int, int>> q;
-		q.insert(make_pair(0, 0));
-
-		for (int i = 0; i < n; ++i) {
-			if (q.empty()) {
-				cerr << "No MST!" << endl;
+			if (pQueue.empty())
 				return {};
+
+			int vertex = pQueue.begin()->second;
+			pQueue.erase(pQueue.begin());
+			used[vertex] = true;
+
+			if (parent[vertex] != -1) {
+				MST[parent[vertex]].push_back(vertex);
+				MST[vertex].push_back(parent[vertex]);
 			}
 
-			auto it = q.begin();
-			int v = it->second;
-			q.erase(it);
+			for (size_t j = 0; j < adjacencyListWithWeight[vertex].size(); ++j) {
+				int to = adjacencyListWithWeight[vertex][j].first;
+				int cost = adjacencyListWithWeight[vertex][j].second;
 
-			if (visited[v]) {
-				continue;
-			}
-			visited[v] = true;
-
-			if (sel_e[v] != -1) {
-				MST[sel_e[v]].push_back(v);
-				MST[v].push_back(sel_e[v]);
-			}
-
-			for (size_t j = 0; j < adjacencyListWithWeight[v].size(); ++j) {
-				int to = adjacencyListWithWeight[v][j].first;
-				int cost = adjacencyListWithWeight[v][j].second;
-
-				if (!visited[to] && cost < min_e[to]) {
-					q.erase(make_pair(min_e[to], to));
-					min_e[to] = cost;
-					sel_e[to] = v;
-					q.insert(make_pair(min_e[to], to));
+				if (cost < minEdgeWeights[to] && !used[to]) {
+					pQueue.erase(std::make_pair(minEdgeWeights[to], to));
+					minEdgeWeights[to] = cost;
+					parent[to] = vertex;
+					pQueue.insert(std::make_pair(minEdgeWeights[to], to));
 				}
 			}
 		}
-
-		return make_pair(MST, make_pair(height, width));
+		return make_pair(MST, std::make_pair(height, width));
 	}
 
 	void printGraph() const override {
 		int count = 0;
-		for (auto i : adjacencyListWithWeight) {
-			cout << count << ": ";
-			for (auto j : i) {
-				cout << "|n: " << j.first << " w: " << j.second << "| ";
+		for (auto& i : adjacencyListWithWeight) {
+			std::cout << count << ": ";
+			for (auto& j : i) {
+				std::cout << "|n: " << j.first << " w: " << j.second << "| ";
 			}
 			count++;
-			cout << endl;
+			std::cout << std::endl;
 		}
 	}
 
@@ -152,29 +109,29 @@ public:
 
 class NoWeightedGraph : public Graph {
 private:
-	vector<vector<int>> adjacencyList;
+	std::vector<std::vector<int>> adjacencyList;
 
 public:
 	NoWeightedGraph() = default;
 
-	NoWeightedGraph(vector<vector<int>> vec, int height, int width) {
+	NoWeightedGraph(std::vector<std::vector<int>>& vec, int& height, int& width) {
 		this->height = height;
 		this->width = width;
 
 		adjacencyList = vec;
 	}
 
-	const vector<vector<int>>& getLinkGraph() {
+	const std::vector<std::vector<int>>& getLinkGraph() {
 		return adjacencyList;
 	}
 
-	void mazeToGraph(const vector<vector<int>>& maze) override {
+	void convertMazeToGraph(const std::vector<std::vector<int>>& maze) override {
 		height = maze.size();
 		width = maze[0].size();
 
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				vector<int> tempVertexAdjacencyList;
+				std::vector<int> tempVertexAdjacencyList;
 				if (maze[i][j] == 0) {
 					if (j - 1 >= 0 && maze[i][j - 1] == 0) {
 						tempVertexAdjacencyList.push_back(i * width + (j - 1));
@@ -194,17 +151,16 @@ public:
 		}
 	}
 
-	// Bfs
-	tuple<vector<int>, int, int> breadthFirstSearch(int start) {
+	std::vector<int> breadthFirstSearch(int start) {
 		if (start == 0) start = width;
 
 		int n = adjacencyList.size();
 		int finish = -1;
 
-		queue<int> queueBurning;
-		vector<bool> used(n);
+		std::queue<int> queueBurning;
+		std::vector<bool> used(n);
 
-		vector<int> length(n), parent(n);
+		std::vector<int> length(n), parent(n);
 
 		queueBurning.push(start);
 		used[start] = true;
@@ -231,45 +187,45 @@ public:
 		}
 
 		if (finish == -1 || !used[finish]) {
-			cout << "No path!" << endl;
+			std::cout << "No path!" << std::endl;
 		}
 		else {
-			vector<int> path;
+			std::vector<int> path;
 			for (int v = finish; v != -1; v = parent[v]) {
 				path.push_back(v);
 			}
 
 			reverse(path.begin(), path.end());
-			return make_tuple(path, height, width);
+			return path;
 
-			cout << "Path: ";
+			/*cout << "Path: ";
 			for (size_t i = 0; i < path.size(); ++i) {
 				cout << path[i] << " ";
 			}
-			cout << endl;
+			cout << endl;*/
 		}
 	}
 
 	void printGraph() const override {
 		for (int i = 0; i < adjacencyList.size(); i++) {
-			cout << i << ": ";
+			std::cout << i << ": ";
 			for (int j = 0; j < adjacencyList[i].size(); j++) {
-				cout << adjacencyList[i][j] << " ";
+				std::cout << adjacencyList[i][j] << " ";
 			}
-			cout << endl;
+			std::cout << std::endl;
 		}
 	}
 };
 
 class Maze {
 private:
-	vector<vector<int>> maze;
+	std::vector<std::vector<int>> maze;
 	int height, width;
 
 public:
 	Maze() = default;
 
-	Maze(vector<vector<int>> vec) {
+	Maze(std::vector<std::vector<int>>& vec) {
 		height = vec.size();
 		width = vec[0].size();
 
@@ -282,23 +238,21 @@ public:
 		}
 	}
 
-	const vector<vector<int>>& getLinkMaze() {
+	const std::vector<std::vector<int>>& getLinkMaze() {
 		return maze;
 	}
 
-	void initializeMaze(int height, int width) {
-		for (int i = 0; i < height; i++) {
-			maze.push_back(vector<int>(width, 1));
-		}
+	void initializeMaze(int& height, int& width) {
+		maze.resize(height, std::vector<int>(width, 1));
 	}
 
 	void enterMaze() {
-		int n = 0; cout << "Enter n: "; cin >> n;
+		int n = 0; std::cout << "Enter n: "; std::cin >> n;
 		initializeMaze(height, width);
 
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				cin >> maze[i][j];
+				std::cin >> maze[i][j];
 			}
 		}
 	}
@@ -307,33 +261,38 @@ public:
 		int countHeightVertex = 5 + rand() % 11;
 		int countWidthVertex = 5 + rand() % 11;
 
-		vector<vector<int>> startMatrix(countHeightVertex, vector<int>(countWidthVertex, 0));
+		std::vector<std::vector<int>> startMatrix(countHeightVertex, std::vector<int>(countWidthVertex, 0));
 
 		WeightedGraph tempGraph;
-		tempGraph.mazeToGraph(startMatrix);
+		tempGraph.convertMazeToGraph(startMatrix);
 		//tempGraph.printGraph();
 
 		auto answer = tempGraph.mstGraph();
 
-		mstGraphToMaze(answer.first, answer.second.first, answer.second.second);
+		convertMstGraphToMaze(answer.first, answer.second.first, answer.second.second);
 	}
 
-	void generateMazeWithParameters(int height, int width) {
-		int countHeightVertex = height / 2;
-		int countWidthVertex = width / 2;
+	void generateMaze(int height, int width) {
+		if (height % 2 == 0)
+			height--;
+		if (width % 2 == 0)
+			width--;
 
-		vector<vector<int>> startMatrix(countHeightVertex, vector<int>(countWidthVertex, 0));
+		int countHeightVertex = (height - 1) / 2;
+		int countWidthVertex = (width - 1) / 2;
+
+		std::vector<std::vector<int>> startMatrix(countHeightVertex, std::vector<int>(countWidthVertex, 0));
 
 		WeightedGraph tempGraph;
-		tempGraph.mazeToGraph(startMatrix);
+		tempGraph.convertMazeToGraph(startMatrix);
 		//tempGraph.printGraph();
 
 		auto answer = tempGraph.mstGraph();
 
-		mstGraphToMaze(answer.first, answer.second.first, answer.second.second);
+		convertMstGraphToMaze(answer.first, answer.second.first, answer.second.second);
 	}
 
-	void mstGraphToMaze(const vector<vector<int>>& adjacencyList, int height, int width) {
+	void convertMstGraphToMaze(const std::vector<std::vector<int>>& adjacencyList, int& height, int& width) {
 		this->height = height * 2 - 1;
 		this->width = width * 2 - 1;
 		initializeMaze(this->height, this->width);
@@ -342,7 +301,7 @@ public:
 		for (int i = 0; i < this->height; i += 2) {
 			for (int j = 0; j < this->width; j += 2) {
 				maze[i][j] = 0;
-				vector<int> vecVex = adjacencyList[aL];
+				std::vector<int> vecVex = adjacencyList[aL];
 				stable_sort(vecVex.begin(), vecVex.end());
 
 				for (int k = 0; k < vecVex.size(); k++) {
@@ -363,9 +322,7 @@ public:
 						maze[i + 1][j] = 0;
 						maze[i + 2][j] = 0;
 					}
-
 				}
-
 				aL++;
 			}
 		}
@@ -381,96 +338,71 @@ public:
 			else maze[i].push_back(1);
 		}
 
-		vector<int> border(maze[0].size(), 1);
+		std::vector<int> border(maze[0].size(), 1);
 
 		maze.insert(maze.begin(), border);
 		maze.push_back(border);
-
-		/*for (int i = 0; i < this->height; i++) {
-			for (int j = 0; j < this->width; j++) {
-				cout << maze[i][j] << " ";
-			}
-			cout << endl;
-		}*/
-
 	}
 
-	void graphToMaze(const vector<vector<int>>& adjacencyList, int height, int width) {
-		this->height = height;
-		this->width = width;
-		initializeMaze(height, width);
+	void convertGraphToMaze(const std::vector<std::vector<int>>& adjacencyList) {
 
-		int i = 0, j = 0, currentI = 0;
-		while (((i + 1) * (j + 1)) < adjacencyList.size()) {
-			if (!adjacencyList[currentI].empty()) {
-				maze[i][j] = 0;
-			}
-			else {
-				maze[i][j] = 1;
-			}
+		for (int index = 0; index < adjacencyList.size(); index++) {
+			int i = index / width;
+			int j = index % width;
 
-
-			if ((j + 1) % width == 0) {
-				i++;
-				j = 0;
-			}
-			else {
-				j++;
-			}
-			currentI++;
+			maze[i][j] = !adjacencyList[index].empty() ? 0 : 1;
 		}
-		maze[height - 1][width - 1] = 1;
 	}
 
 	void printMaze() {
-		cout << height << "x" << width << endl;
+		std::cout << height << "x" << width << std::endl;
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				cout << (maze[i][j] == 1 ? u8"\u25A0" : " ") << " ";
+				std::cout << (maze[i][j] == 1 ? u8"\u25A0" : " ") << " ";
 			}
-			cout << endl;
+			std::cout << std::endl;
 		}
 	}
 
-	tuple<bool, int> vertexInPath(vector<int> path, int vertex) {
+	std::pair<bool, int> vertexInPath(std::vector<int>& path, int& vertex) {
 		for (int i = 0; i < path.size(); i++) {
 			if (path[i] == vertex) {
 				if (i == 0) {
-					return tuple<bool, int>(true, 1);
+					return std::make_pair(true, 1);
 				}
 				else if (i == path.size() - 1) {
-					return tuple<bool, int>(true, 2);
+					return std::make_pair(true, 2);
 				}
 				else {
-					return tuple<bool, int>(true, 0);
+					return std::make_pair(true, 0);
 				}
 			}
 		}
-		return tuple<bool, int>(false, 0);
+		return std::make_pair(false, 0);
 	}
 
-	void printMazeWithPath(vector<int> path) {
+	void printMazeWithPath(std::vector<int>& path) {
 		int countPath = 0;
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
 				auto answer = vertexInPath(path, countPath);
-				if (get<0>(answer)) {
-					if (get<1>(answer) == 1) {
-						cout << 'S' << " ";
+				if (answer.first) {
+					if (answer.second == 1) {
+						std::cout << 'S' << " ";
 					}
-					else if (get<1>(answer) == 2) {
-						cout << 'F' << " ";
+					else if (answer.second == 2) {
+						std::cout << 'F' << " ";
 					}
 					else {
-						cout << u8"\u25CB" << " ";
+						std::cout << u8"\u25CB" << " ";
 					}
 				}
 				else {
-					cout << (maze[i][j] == 1 ? u8"\u25A0" : " ") << " ";
+					std::cout << (maze[i][j] == 1 ? u8"\u25A0" : " ") << " ";
 				}
 				countPath++;
 			}
-			cout << endl;
+			std::cout << std::endl;
 		}
 	}
 
@@ -481,29 +413,24 @@ int main() {
 
 	srand(static_cast<unsigned int>(time(nullptr)));
 
-	//maze.enterMaze();
-
 	Maze maze;
+	//maze.enterMaze();
 	maze.generateMaze();
 
 	/*int height = 0, width = 0; cout << "Enter height and width: "; cin >> height >> width;
-	maze.generateMazeWithParameters(height, width);*/
+	maze.generateMaze(height, width);*/
 
 	maze.printMaze();
-	cout << "-----------" << endl;
+	std::cout << "-----------" << std::endl;
 
 	NoWeightedGraph graph;
-	graph.mazeToGraph(maze.getLinkMaze());
+	graph.convertMazeToGraph(maze.getLinkMaze());
 	//graph.printGraph();
 	//cout << "-----------" << endl;
 
-	//int start = 0; cout << "Enter start position: "; cin >> start;
-	auto path = graph.breadthFirstSearch(0);
+	std::vector<int> path = graph.breadthFirstSearch(0);
 
-	Maze maze1;
-	maze1.graphToMaze(graph.getLinkGraph(), get<1>(path), get<2>(path));
-	//maze1.printMaze();
-	maze1.printMazeWithPath(get<0>(path));
+	maze.printMazeWithPath(path);
 
 	return 0;
 }
