d640fb65380d74d72116def6536c07194c02cdcc
Mikhaeris <mikhail.kiselev.dev@gmail.com>
2025-04-08 22:05:05 +0300

    some changes

--- Laboratory/APDDS/Laba12/Laba12.cpp
+++ Laboratory/APDDS/Laba12/Laba12.cpp
@@ -3,45 +3,24 @@
 
 using namespace std;
 
-struct DirVec {
-    int dirX, dirY;
-
-    bool operator==(const DirVec& other) const {
-        return ((dirX == other.dirX) && (dirY == other.dirY));
-    }
-};
-
-bool checkLineCrossing(const DirVec& dirF, const DirVec& dirS) {
-    return (dirF.dirX * dirS.dirY - dirF.dirY * dirS.dirX) != 0;
-}
-
-int countAreas(vector<pair<DirVec, bool>>& lines, DirVec& firstLine) {
-    int countPoint = 0;
-    for (const auto& senondLine : lines) {
-        if (!(firstLine == senondLine.first) && checkLineCrossing(firstLine, senondLine.first) && secondLine.second == false) {
-            countPoint++;
-            secondLine.second = true;
-        }
-    }
-
-    return countPoint;
-}
-
 int main() {
-    int countGlobalPoints = 0;
-    vector<pair<DirVec, bool>> lines;
+	int countLine = 0; cout << "Enter N: "; cin >> countLine;
 
-    int countLine = 0; cout << "Enter N: "; cin >> countLine;
+	vector<int> A(countLine), B(countLine);
 
-    int tempY1 = 0, tempY2 = 0;
-    for (int i = 0; i < countLine - 1; i++) {
-        cin >> tempY1 >> tempY2;
-        DirVec newLine{ 5, tempY2 - tempY1 };
+	for (int i = 0; i < countLine; i++) {
+		cin >> A[i] >> B[i];
+	}
 
-        countGlobalPoints += countAreas(lines, newLine);
-        lines.push_back(newLine);
-    }
+	int countPoint = 0;
+	for (int i = 0; i < countLine; i++) {
+		for (int j = 0; j < countLine; j++) {
+			if (A[i] < A[j] && B[i] > B[j]) {
+				countPoint++;
+			}
+		}
+	}
 
-    cout << (countLine - 1) + countGlobalPoints + 1 << endl;
-    return 0;
+	cout << countLine + countPoint + 1 << endl;
+	return 0;
 }
