bedbd102212bfa4ec4a613c323108e8fdd54fd3f
Mikhaeris <mikhail.kiselev.dev@gmail.com>
2025-05-14 23:25:38 +0300

    some changes

--- Laboratory/APDDS/Laba9/Laba9Last.cpp
+++ Laboratory/APDDS/Laba9/Laba9Last.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <queue>
 
@@ -70,3 +70,105 @@ int main() {
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+Option Explicit
+Const p = 4                         '   ïîòîê êëèåíòîâ
+Const TotalTime = 60                '   îáùåå âðåìÿ
+Const t = 10                        '   âðåìÿ îáñëóæèâàíèÿ îäíîãî êëèåíòà
+Const nClient = TotalTime \ p + 1   '   ìàêñèìàëüíîå êîëè÷åñòâî êëèåíòîâ
+
+Type DataClient                     '   äàííûå êëèåíòà
+    Number As Integer               '   íîìåð êëèåíòà
+    Next As Integer                 '   ññûëêà íà ñëåäóþùåãî
+    SummaT As Integer               '   âðåìÿ, ïðîâåäåííîå â î÷åðåäè
+End Type
+
+Dim Sequence(nClient) As DataClient     ' ìàññèâ êëèåíòîâ (î÷åðåäü)
+Dim KassaFree As Boolean                ' ñâîáîäíà ëè êàññà
+Dim KassaTime As Integer                '
+Dim kClient As Integer                  ' òåêóùåå êîëè÷åñòâî êëèåíòîâ
+Dim Tail As Integer                     ' óêàçàòåëü íà õâîñò î÷åðåäè
+Dim SummaTime As Integer, Average As Single
+'
+Sub AddToSequence(kClient)
+    With Sequence(kClient)
+        .Number = kClient
+        .Next = 0
+        .SummaT = 0
+    End With
+    Sequence(kClient - 1).Next = kClient
+    Tail = kClient
+End Sub
+'
+Sub SdvigSequence()
+    Sequence(0).Next = Sequence(0).Next + 1
+    KassaFree = False
+End Sub
+'
+Sub Main()
+Dim i As Integer, j As Integer
+
+With Sequence(0)
+    .Number = 0
+    .SummaT = 0
+    .Next = 1
+End With
+
+Tail = 0
+KassaFree = True
+KassaTime = 0
+kClient = 0
+
+For i = 1 To TotalTime
+    If i Mod p = 1 Then
+        kClient = kClient + 1
+        AddToSequence (kClient)
+    End If
+    
+    If KassaTime = t Then
+        KassaFree = True
+        KassaTime = 0
+    End If
+    
+    If KassaFree = False Then
+        KassaTime = KassaTime + 1
+    End If
+    
+    If KassaFree Then
+         SdvigSequence
+         KassaFree = False
+         KassaTime = 1
+    End If
+
+    For j = Sequence(0).Next To Tail
+        Sequence(j).SummaT = Sequence(j).SummaT + 1
+    Next
+
+    Cells(i + 3, 15) = Sequence(0).Next
+    Cells(i + 3, 16) = Tail
+    Cells(i + 3, 17) = KassaTime
+
+    For j = Sequence(0).Next To Tail
+        Cells(i + 3, j + 17) = Sequence(j).Number
+    Next
+Next
+
+SummaTime = 0
+For i = 1 To Tail
+    SummaTime = SummaTime + Sequence(i).SummaT
+    Cells(i + 10, 12) = Sequence(i).SummaT
+Next
+Average = SummaTime / kClient
+Cells(3, 13) = Average
+    
+End Sub
+
