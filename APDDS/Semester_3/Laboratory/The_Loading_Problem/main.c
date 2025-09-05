da436052f7c6c3f9280f9dbb777f916088c69473
mikhaeris <mikhail.kiselev.dev@gmail.com>
2025-09-05 12:50:37 +0300

    fix(APDDS): implemented all functions in laba - the_loading_problem

--- APDDS/Semester_3/Laboratory/The_Loading_Problem/main.c
+++ APDDS/Semester_3/Laboratory/The_Loading_Problem/main.c
@@ -1,6 +1,3 @@
-/* TODO: 1) Дописать проверку комбинаций, учитывая 11 продук(10 бит) и перевозку товара 1 со 2
- *       2) Написать функцию вывода ответа(количества комбинаций и первую комбинацию и т.д.)
- *       2) Написать функцию вывода всех комбинаций с суммой */
 #include <stdio.h>
 
 #define COUNT_CARGO 20
@@ -9,6 +6,7 @@
 
 int getAllCombinations(int *arr, int *goods);
 void checkCombinations(int *arr, int count, int *goods);
+void printAllCombinations(int *arr, int count);
 
 int main() {
     int goods[COUNT_CARGO] = {
@@ -18,28 +16,43 @@ int main() {
     static int max_comb[ALL_COMBINATIONS] = {0};
     int count_valid_combinations = getAllCombinations(max_comb, goods);
 
-    // printf("cargo weight: %d\n", max_sum);
     printf("count comb: %d\n", count_valid_combinations);
-    printf("some combination: ");
 
-    int check_sum = 0;
-    for (int i = 0; i < COUNT_CARGO; ++i) {
-        if (max_comb[0] & (1 << i)) {
-            printf("%d ", i+1);
-            check_sum += goods[i];
-        }
+    printf("check combinations? [y/N]: ");
+    char c = getchar();
+    if (c == 'y') {
+        getchar();
+        puts("");
+        checkCombinations(max_comb, count_valid_combinations, goods);
+        puts("");
     }
-    printf("\n%d\n", check_sum);
+    c = 0;
 
-    /* printf("all combinations\n");
-    for (int *i = max_comb; i < pc; ++i) {
-        for (int j = 0; j < COUNT_CARGO; ++j) {
-            if (*i & (1 << j)) {
-                //printf("%d ", j+1);
+    printf("print all combinations? [y/N]: ");
+    c = getchar();
+    if (c == 'y') {
+        getchar();
+        puts("");
+        printAllCombinations(max_comb, count_valid_combinations);
+        puts("");
+    }
+    c = 0;
+
+    printf("print random combination? [y/N]: ");
+    c = getchar();
+    if (c == 'y') {
+        getchar();
+        puts("");
+        printf("combination: ");
+        int check_sum = 0;
+        for (int i = 0; i < COUNT_CARGO; ++i) {
+            if (max_comb[1] & (1 << i)) {
+                printf("%d ", i+1);
+                check_sum += goods[i];
             }
         }
-        printf("\n");
-    } */
+        printf("\nsum: %d\n", check_sum);
+    }
 
     return 0;
 }
@@ -65,7 +78,7 @@ int getAllCombinations(int *arr, int* goods) {
         /* так как товар 11 может быть раздроблен,
          * то его можно добавить текущую сумму */
         char flag = 0;
-        if (curr_sum < TARGET && (TARGET - curr_sum) <= goods[10]) {
+        if (curr_sum < TARGET && (TARGET - curr_sum) <= goods[10] && !(mask & (1 << 10))) {
             curr_sum += TARGET - curr_sum;
             flag = 1;
         }
@@ -85,12 +98,53 @@ int getAllCombinations(int *arr, int* goods) {
 }
 
 /* check combinations */
-void checkCombinations(int *arr, int count) {
-    for (int i = 0; i < count; i++) {
+void checkCombinations(int *arr, int count, int *goods) {
+    char flag = 1;
+    for (int i = 0; i < count; ++i) {
+        int curr_sum = 0;
         for (int j = 0; j < COUNT_CARGO; ++j) {
             if (arr[i] & (1 << j)) {
-
+                curr_sum += goods[j];
             }
         }
+
+        if (curr_sum < 600) {
+            printf("error: combination %d less then %d\n", i, TARGET);
+            flag = 0;
+        }
+
+        if (curr_sum > 600) {
+            if (!(arr[i] & (1 << 10))) {
+                printf("error: combination %d more then %d and 11 good don't pick\n", i, TARGET);
+                flag = 0;
+            }
+
+            if ((arr[i] & (1 << 10)) && curr_sum > TARGET+goods[10]) {
+                printf("error: combination %d with 11 good, but sum more than %d\n", i, TARGET+goods[10]);
+                flag = 0;
+            }
+        }
+
+        if ((arr[i] & (1 << 0)) && !(arr[i] & (1 << 1))) {
+            printf("error: combination %d with good 1, but don't have good 2", i);
+            flag = 0;
+        }
+    }
+
+    if (flag) {
+        printf("all combinations good!\n");
+    }
+}
+
+void printAllCombinations(int *arr, int count) {
+    printf("all combinations:\n");
+    for (int i = 0; i < count; ++i) {
+        printf("combination %d", i);
+        for (int j = 0; j < COUNT_CARGO; ++j) {
+            if (arr[i] & (1 << j)) {
+                printf("%d ", j+1);
+            }
+        }
+        printf("\n");
     }
 }
